1d7c5a6b4bd1b459f819111d8f1903ab8e161cf2
NilimaSardar <122077945+NilimaSardar@users.noreply.github.com>
2024-08-25 14:25:59 +0000

    known about strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,43 @@
+const name = "Nilima"
+const repoCount = 6
+
+// console.log(`Hello my name is ${name} and the repo Count is ${repoCount}`);
+
+const gameName = new String("Nilimasb");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('s'));
+
+// const newString = gameName.substring(0,5);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4);
+// console.log(anotherString);
+
+// const newStringOne = "     nilima    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://nilima.com/nilima%02sardar";
+console.log(url.replace("%02","-"));
+
+console.log(url.includes('nilima'));
+console.log(url.includes('hello'));
+
+const meName = "hello-me-nilima-sardar";
+console.log(meName.split('-'));
+
+
+
+
+
+
+
+
+
+
