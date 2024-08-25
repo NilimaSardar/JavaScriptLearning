9b807b283bd75396d19a0009707f697f2493c30d
NilimaSardar <122077945+NilimaSardar@users.noreply.github.com>
2024-08-25 06:33:03 +0000

    stack and heap memory Read

---
+++ 01_basics/05_StackAndHeapMemory.js
@@ -0,0 +1,27 @@
+//Stack(Primitive),Heap(Non-Primitive)
+
+// let myYoutubename ="nilimasardardotcom";
+
+// let anothername = myYoutubename;
+// console.log(anothername);//nilimasardardotcom
+
+// let myYoutubename ="nilimasardardotcom";
+
+// let anothername = myYoutubename;
+// anothername = "YoursNilima";
+
+// console.log(myYoutubename);//nilimasardardotcom
+// console.log(anothername);//YoursNilima
+
+let userOne = {
+    email:"user@goggle.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email="Nilima@goggle.com"
+console.log(userOne.email);//Nilima@goggle.com
+console.log(userTwo.email);//Nilima@goggle.com
+
+
