dffc488ceb5b9cc40d19b03d58b16d3255e634ca
NilimaSardar <122077945+NilimaSardar@users.noreply.github.com>
2024-08-25 06:07:47 +0000

    learnt comparison operator

---
+++ 01_basics/03_comparisonOperation.js
@@ -0,0 +1,67 @@
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);//-3
+
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//8
+// console.log(2/3);//0.666666666666
+// console.log(2%3);//2
+
+let str1 = "hello";
+let str2 = " Nilima!"
+
+let str3 = str1 + str2;
+// console.log(str3);//hello Nilima!
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+2+"2");//52
+
+// console.log(+true);//1
+// console.log(+"");//0
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);//101
+
+// let x=2;
+// let y=x++;
+
+// console.log("x:"+x +" y:"+y);
+
+// let a=2;
+// let b=++a;
+
+// console.log("a:"+a +" b:"+b);
+
+
+// comparison of datatypes
+// console.log(2>1);//true
+// console.log(2>=1);//true
+// console.log(2<1);//false
+// console.log(2==1);//false
+// console.log(2!=1);//true
+
+// console.log("2">1);//true
+// console.log("02">1);//true
+
+// console.log(null>0);//false
+// console.log(null==0);//flase
+// console.log(null>=0);//true
+
+/* The reason is that an equality check== and comparisons <>>=<= work differently. Comparisons convert null to a number, treating it as 0
+
+That's why null>=0 is true and null>0 is false
+*/
+
+
+// console.log(undefined>0);//false
+// console.log(undefined==0);//flase
+// console.log(undefined>=0);//false
+
+console.log("2"===2);//false->Compares datatype
