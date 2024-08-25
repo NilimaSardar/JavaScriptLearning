011f7252c019cd42f4329a67c9372b272b388d76
NilimaSardar <122077945+NilimaSardar@users.noreply.github.com>
2024-08-25 05:24:08 +0000

    changed test.js to new name and learnt conversion operation

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,61 @@
+"use strict"; //treat all JScode as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let myName ="Nilima";
+let address = "Biratnagar";
+let phone = 9823242342;
+let degree = null;
+let isLoggedIn = true;
+let gender;
+
+//console.log(typeof degree);//object
+//console.log(typeof gender);//undefined
+//console.log(typeof phone);//number
+//console.log(typeof myName);//string
+//console.log(typeof isLoggedIn);//boolean
+
+let score = "33";
+// console.log(typeof score);//string
+// console.log(typeof(score));//string
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//number
+
+
+// score = "33abc";
+// score = undefined;
+// score = "Nilima";
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//NaN
+
+
+// score = null;
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//0
+
+
+// score = true;
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//number
+// console.log(valueInNumber);//1
+
+//false => 0
+
+
+isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);//true
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);//33
+console.log(typeof stringNumber);//string
+
+
+
+
