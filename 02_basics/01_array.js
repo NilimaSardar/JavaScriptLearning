eda93033b85996504d277ed16c0f1acae21723dc
NilimaSardar <122077945+NilimaSardar@users.noreply.github.com>
2024-08-26 13:08:48 +0000

    array basics covered

---
+++ 02_basics/01_array.js
@@ -0,0 +1,49 @@
+const myArr=[0,1,2,3,4,5]
+// const myHeros = ["shaktiman","naagraj"]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr);
+// console.log(myHeros);
+// console.log(myArr2);
+
+//Array Methods
+
+// myArr.push(52);//add in last index
+// console.log(myArr);
+// myArr.pop();//delete from last index
+// console.log(myArr);
+myArr.unshift(23);//add in first index
+console.log(myArr);
+myArr.shift();//delete in first index
+console.log(myArr);
+
+
+
+
+// console.log(myArr.includes(5));
+
+// console.log(myArr.indexOf(8));
+
+// let newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// /* Slice and Splice */ 
+// console.log("S L I C E");
+
+// console.log("A ",myArr);
+// const myn1 = myArr.slice(1,3);
+// console.log(myn1);
+// console.log("B", myArr);
+
+// console.log("\n\n");
+
+
+
+// console.log("S P L I C E");
+
+// console.log("A ",myArr);
+// const myn2 = myArr.splice(1,3);
+// console.log(myn2);
+// console.log("B", myArr);
